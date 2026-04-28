058a42d15cf026f7c394352cd82ff47f64f6bc37
Maxi Ortega <maximilianortega2000@gmail.com>
2026-04-28 14:29:50 -0300

    chore: setup env variables and gitignore

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'TU_URL'
-const supabaseAnonKey = 'TU_ANON_KEY'
+// Vite cargará automáticamente estos valores desde tu archivo .env (en local)
+// o desde las variables de entorno que configures en Vercel.
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
